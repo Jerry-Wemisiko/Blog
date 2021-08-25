8ffc51ace0e6c27d88577b28f2e55dc4a8678f32
Jerry-Wemisiko <jerry.wemisiko@student.moringaschool.com>
2021-08-25 14:55:10 +0300

    registered auth blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import    SQLAlchemy
 from flask_mail import Mail
+from config import config_options
 
 bootstrap = Bootstrap()
 db = SQLAlchemy ()
@@ -19,6 +20,9 @@ def create_app(config_name):
     db.init_app(app)
     mail.init_app(app)
 
+    #Register blueprints
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
 
 
     return app
