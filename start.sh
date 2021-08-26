f52c308ca59e8c25875fb5f2132aafaae41f053e
JSkyles <82439539+Jerry-Wemisiko@users.noreply.github.com>
2021-08-26 18:24:16 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 export MAIL_USERNAME='skyles171@gmail.com'
-export MAIL_PASSWORD='Benzs680'
+export MAIL_PASSWORD='SKlassen'
 export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa:123456@localhost/blog'
 
 export SECRET_KEY='secret'
